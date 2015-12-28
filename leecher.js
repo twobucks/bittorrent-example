d4c9929302329ca2465eb7ca5d2c2d1d25ecef33
Hrvoje Šimić <shime@twobucks.co>
2015-12-28 19:37:02 +0100

    add "hello world" message

--- leecher.js
+++ leecher.js
@@ -1,46 +1,15 @@
-var Client = require('bittorrent-tracker')
-var parseTorrent = require('parse-torrent')
-var fs = require('fs')
+var WebTorrent = require('webtorrent')
 
-var torrent = fs.readFileSync('foo.torrent')
-var parsedTorrent = parseTorrent(torrent) // { infoHash: 'xxx', length: xx, announce: ['xx', 'xx'] }
+var client = new WebTorrent()
+var magnetUri = "magnet:?xt=urn:btih:b8e8adbee320fc312e5fd71479329c3d53c40aea&dn=message&tr=http%3A%2F%2Flocalhost%3A8000%2Fannounce"
 
-var peerId = new Buffer('01234567890123456780')
-var port = 7442
+client.add(magnetUri, function (torrent) {
+  // Got torrent metadata!
+  console.log('Client is downloading:', torrent.infoHash)
 
-var client = new Client(peerId, port, parsedTorrent)
-
-client.on('error', function (err) {
-  console.log('error');
-  // fatal client error!
-  console.log(err.message)
+  torrent.files.forEach(function (file) {
+    file.getBuffer(function (er, buf) {
+      console.log(buf.toString())
+    })
+  })
 })
-
-client.on('warning', function (err) {
-  console.log('warning');
-  // a tracker was unavailable or sent bad data to the client. you can probably ignore it
-  console.log(err.message)
-})
-client.on('update', function (data) {
-  console.log('update')
-  console.log(data)
-  console.log('got an announce response from tracker: ' + data.announce)
-  console.log('number of seeders in the swarm: ' + data.complete)
-  console.log('number of leechers in the swarm: ' + data.incomplete)
-})
-
-client.once('peer', function (addr) {
-  console.log('peer')
-  console.log('found a peer: ' + addr)
-})
-
-client.on('scrape', function (data) {
-  console.log('got a scrape response from tracker: ' + data.announce)
-  console.log('number of seeders in the swarm: ' + data.complete)
-  console.log('number of leechers in the swarm: ' + data.incomplete)
-  console.log('number of total downloads of this torrent: ' + data.incomplete)
-})
-
-setInterval(function(){
-  client.start()
-}, 1000)
--- seeder.js
+++ seeder.js
@@ -1,49 +1,5 @@
-var Client = require('bittorrent-tracker')
-var parseTorrent = require('parse-torrent')
-var fs = require('fs')
-
-var torrent = fs.readFileSync('foo.torrent')
-var parsedTorrent = parseTorrent(torrent) // { infoHash: 'xxx', length: xx, announce: ['xx', 'xx'] }
-
-var peerId = new Buffer('13234567890123456789')
-var port = 7444
-
-var client = new Client(peerId, port, parsedTorrent)
-
-client.on('error', function (err) {
-  console.log('error');
-  // fatal client error!
-  console.log(err.message)
-})
-
-client.on('warning', function (err) {
-  console.log('warning');
-  // a tracker was unavailable or sent bad data to the client. you can probably ignore it
-  console.log(err.message)
-})
-client.on('update', function (data) {
-  console.log('update')
-  console.log(data)
-  console.log('got an announce response from tracker: ' + data.announce)
-  console.log('number of seeders in the swarm: ' + data.complete)
-  console.log('number of leechers in the swarm: ' + data.incomplete)
-})
-
-client.once('peer', function (addr) {
-  console.log('peer')
-  console.log('found a peer: ' + addr) // 85.10.239.191:48623
-})
-
-client.on('scrape', function (data) {
-  console.log('got a scrape response from tracker: ' + data.announce)
-  console.log('number of seeders in the swarm: ' + data.complete)
-  console.log('number of leechers in the swarm: ' + data.incomplete)
-  console.log('number of total downloads of this torrent: ' + data.incomplete)
-  console.log(data)
-})
-
-client.complete() // start seeding?
-client.scrape()
-setInterval(function(){
-  client.start()
+var WebTorrent = require('webtorrent')
+var client = new WebTorrent()
+client.seed('message', {announceList: [["http://localhost:8000/announce"]]}, function(torrent){
+  console.log('Client is seeding:', torrent.magnetURI)
 })
